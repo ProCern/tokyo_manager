c26011a5044a40176a82068e64085ec61e8e229c
Adam Vaughan <ajv@absolute-performance.com>
2012-01-31 14:32:08 -0700

    add spec for connection manager module

--- lib/tokyo_manager/connection_manager.rb
+++ lib/tokyo_manager/connection_manager.rb
@@ -23,12 +23,16 @@ module TokyoManager
       begin
         connection = TokyoTyrant::DB.new(host, port)
       rescue TokyoTyrantErrorRefused
-        # if no database exists for the given date, fall back to the legacy database on the default port
-        connection = TokyoTyrant::DB.new(host, default_port)
+        begin
+          # if no database exists for the given date, fall back to the legacy database on the default port
+          connection = TokyoTyrant::DB.new(host, default_port)
+        rescue TokyoTyrantErrorRefused
+          connection = nil
+        end
       end
 
       unless connection
-        raise "Failed to connect to TokyoTyrant at #{host}:#{port} - #{connection.errmsg(connection.ecode)}"
+        raise "Failed to connect to TokyoTyrant at #{host}:#{port}"
       end
 
       connection
---
+++ spec/tokyo_manager/connection_manager_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe TokyoManager::ConnectionManager do
+  let(:date) { Date.new(2012, 1, 31) }
+
+  describe "connection_for_date" do
+    context "when an instance is running for the date" do
+      let(:connection) { Object.new }
+
+      before do
+        TokyoTyrant::DB.should_receive(:new).with('localhost', 10527).and_return(connection)
+      end
+
+      it "returns a connection to the instance" do
+        TokyoManager.connection_for_date(date).should be(connection)
+      end
+    end
+
+    context "when an instance is not running for the date" do
+      context "when an instance is running on the default port" do
+        let(:connection) { Object.new }
+
+        before do
+          TokyoTyrant::DB.should_receive(:new).with('localhost', 10527).and_raise(TokyoTyrantErrorRefused)
+          TokyoTyrant::DB.should_receive(:new).with('localhost', 1978).and_return(connection)
+        end
+
+        it "returns a connection to the default instance" do
+          TokyoManager.connection_for_date(date).should be(connection)
+        end
+      end
+
+      context "when an instance is not running on the default port" do
+        before do
+          TokyoTyrant::DB.should_receive(:new).with('localhost', 10527).and_raise(TokyoTyrantErrorRefused)
+          TokyoTyrant::DB.should_receive(:new).with('localhost', 1978).and_raise(TokyoTyrantErrorRefused)
+        end
+
+        it "raises an error" do
+          lambda { TokyoManager.connection_for_date(date) }.should raise_error('Failed to connect to TokyoTyrant at localhost:10527')
+        end
+      end
+    end
+  end
+end
